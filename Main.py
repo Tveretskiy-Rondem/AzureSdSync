ef1691625d0230596c1ce515d5f43113d55de90e
Tveretskiy-Rondem <a.tveretskiy@rondem.ru>
2023-12-12 19:14:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,44 +16,45 @@ else:
 
 while True:
     print("Iteration:", iteration)
+
     try:
-        print("SD to DB")
+        print("SD to DB", flush=True)
         with open(path + "SdToDb.py") as sdtodb:
             exec(sdtodb.read())
-        print("SD status checker")
+        print("SD status checker", flush=True)
         with open(path + "SdStatusChecker.py") as statuschecker:
             exec(statuschecker.read())
-        print("SD info updater")
+        print("SD info updater", flush=True)
         with open(path + "SdUpdateInfo.py") as sdupdate:
             exec(sdupdate.read())
     except Exception as error:
         print("---------------", "WARNING!", end='\n')
         print("Exception on SD block!")
-        print("An exception occurred:", error)
+        print("An exception occurred:", error, flush=True)
 
     try:
-        print("Azure work items list to DB")
+        print("Azure work items list to DB", flush=True)
         with open(path + "AzureWorkItemsListToDb.py") as azurelistdb:
             exec(azurelistdb.read())
-        print("Azure work items content to DB")
+        print("Azure work items content to DB", flush=True)
         with open(path + "AzureToDb.py") as azuretodb:
             exec(azuretodb.read())
-        print("Azure info updater")
+        print("Azure info updater", flush=True)
         with open(path + "AzureUpdateInfo.py") as azureupdate:
             exec(azureupdate.read())
-        print("Azure status checker")
+        print("Azure status checker", flush=True)
         with open(path + "AzureStatusChecker.py") as azurestatuschecker:
             exec(azurestatuschecker.read())
-        print("Matcher")
+        print("Matcher", flush=True)
         with open(path + "AzureSdMatch.py") as match:
             exec(match.read())
     except Exception as error:
         print("---------------", "WARNING!", end='\n')
         print("Exception on Azure block!")
-        print("An exception occurred:", error)
+        print("An exception occurred:", error, flush=True)
 
     try:
-        print("Initial review")
+        print("Initial review", flush=True)
         with open(path + "InitialReview.py") as initial:
             exec(initial.read())
         # print("Closed in SD")
@@ -65,6 +66,6 @@ while True:
     except Exception as error:
         print("---------------", "WARNING!", end='\n')
         print("Exception on Main logic block!")
-        print("An exception occurred:", error)
+        print("An exception occurred:", error, flush=True)
 
     iteration = iteration + 1
