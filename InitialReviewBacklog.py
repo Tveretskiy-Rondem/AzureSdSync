a0ad5855b7dc59adddc4698a4d05d2e2a301658f
Tveretskiy-Rondem <a.tveretskiy@rondem.ru>
2024-01-26 23:12:43 +0300

    Second attach request with IP

--- InitialReviewBacklog.py
+++ InitialReviewBacklog.py
@@ -12,35 +12,46 @@ dbCreds = Vars.dbCreds
 workItemsBacklog = Functions.dbQuerySender(dbCreds, "SELECT", "SELECT id FROM azure_statuses WHERE status = 'Backlog' AND old_status != '' AND is_last = true")
 workItemsBacklog = Functions.responseToOneLevelArray(workItemsBacklog)
 
-# Проверка на наличие связанной задачи в SD, проверка на статус в sd:
-workItemsBySd = []
+# Проверка на запись last_action = InitialReview
+workItemsIR = []
 for workItemId in workItemsBacklog:
-    print("For1", workItemId)
+    isInitialReview = Functions.dbQuerySender(dbCreds, "EXISTS", "SELECT EXISTS (SELECT * FROM azure_work_items WHERE id = " + str(workItemId) + " AND last_action = 'Initial review')")
+    if isInitialReview:
+        workItemsIR.append(workItemId)
+
+# Проверка на наличие связанной задачи в SD, получение списка задач в SD, проверка на статус в sd:
+sdIssues = []
+for workItemId in workItemsIR:
     linkedIssueId = Functions.dbQuerySender(dbCreds, "SELECT", "SELECT sd_issue_id FROM azure_sd_match WHERE azure_work_item_id = " + str(workItemId))
     if linkedIssueId != []:
         linkedIssueId = linkedIssueId[0][0]
-        print("Linked issue id flat:", linkedIssueId)
         isLinkedIssueBacklog = Functions.dbQuerySender(dbCreds, "EXISTS", "SELECT EXISTS (SELECT * FROM sd_statuses WHERE id = " + str(linkedIssueId) + " AND status = 'Бэклог' AND is_last = true)")
         if isLinkedIssueBacklog:
-            workItemsBySd.append(workItemId)
+            sdIssues.append(linkedIssueId)
 
-# Проверка на запись last_action = InitialReview
-workItemsIR = []
-for workItemId in workItemsBySd:
-    print("For2", workItemId)
-    isInitialReview = Functions.dbQuerySender(dbCreds, "EXISTS", "SELECT EXISTS (SELECT * FROM azure_work_items WHERE id = " + str(workItemId) + " AND last_action = 'Initial review')")
-    print("SELECT EXISTS (SELECT * FROM azure_work_items WHERE id = " + str(workItemId) + " AND last_action = 'Initial review')")
-    print(workItemId, isInitialReview)
-    if isInitialReview:
-        workItemsIR.append(workItemId)
-print(workItemsIR)
+print(sdIssues)
 
-# # Todo проверка статуса в SD != backlog (вынесено в первый блок проверки)
-# workItemsBySdBacklog = []
-# for workItem in workItemsIR:
+# TEST:
+for issueId in sdIssues:
+    print(Functions.dbQuerySender(dbCreds, "SELECT", "SELECT status, last_action FROM sd_issues WHERE id = " + str(issueId)))
+
 
 # Todo изменение статуса в SD:
-
+# Todo поменять токен на свой
+# urlChangeSdStatus = "https://sd.primo-rpa.ru/api/v1/issues/" +  + "/statuses?api_token=ae095dff50035a3dd6fd64405de7bf57c1d08e6e"
+#
+# payloadChangeSdStatus = json.dumps({
+#   "code": "primo_rpa_backlog",
+#   "name": "Бэклог",
+#   "comment": "Тестовый перевод в бэклог.",
+#   "skip_options": [
+#     "skip_triggers",
+#     "skip_notifications",
+#     "skip_webhooks"
+#   ]
+# })
+# headers = {'Content-Type': 'application/json'}
+# response = requests.request("POST", urlChangeSdStatus, headers=headers, data=payloadChangeSdStatus)
 
 # Todo изменение last_action azure_work_items на InitialReviewBacklog
 
